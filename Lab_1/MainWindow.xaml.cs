3a8a3412fe53c49f8b4ee328080a09fc30bec422
IgoninMihail <92269370+IgoninMihail@users.noreply.github.com>
2022-06-08 18:27:15 +0200

    Добавьте файлы проекта.

---
+++ Lab_1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(radius.Text);
+            double y = double.Parse(radian.Text);
+            double z = 2 * 3.14 * x * (y / 360);
+            res.Text = $"{z}";
+        }
+    }
+}
